7cb58b6cd16c210122f816243cb5f992a55eb5e3
Abraham Rodriguez <abrahamr920@gmail.com>
2025-08-01 08:50:47 -0600

    Refactor la clase Conexion para registrar el driver JDBC y mejorar el manejo de excepciones

--- jsf-crud-local/src/main/java/com/abraham/jsf/crud/local/conexion/Conexion.java
+++ jsf-crud-local/src/main/java/com/abraham/jsf/crud/local/conexion/Conexion.java
@@ -1,23 +1,26 @@
 package com.abraham.jsf.crud.local.conexion;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-
     private static final String URL = "jdbc:mysql://localhost:3306/crud_empleados?useSSL=false&serverTimezone=UTC";
+    // private static final String URL =
+    // "jdbc:mysql://localhost:3306/crud_empleados?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
     private static final String USER = "root";
-    private static final String PASS = "sasa1234";
+    private static final String PASS = "sasa1234"; // <-- usá un archivo externo para esto en producción
 
     public static Connection getConexion() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+        } catch (SQLException e) {
             e.printStackTrace();
-            // Podrías lanzar una RuntimeException si quieres que falle rápido
-            throw new SQLException("No se pudo cargar el driver JDBC", e);
         }
+
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
