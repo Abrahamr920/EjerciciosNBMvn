6f97664005d5cbd3583ef4361f8e74d9bf3f2ed2
Abraham Rodriguez <abrahamr920@gmail.com>
2025-08-02 22:39:04 -0600

    Refactor EmpleadoDAO para eliminar la dependencia de JpaUtil y utilizar directamente EntityManager, simplificando la gestión de la persistencia.

--- jsf-crud-local/src/main/java/com/abraham/jsf/crud/local/dao/EmpleadoDAO.java
+++ jsf-crud-local/src/main/java/com/abraham/jsf/crud/local/dao/EmpleadoDAO.java
@@ -2,82 +2,49 @@ package com.abraham.jsf.crud.local.dao;
 
 import java.util.List;
 
-import javax.enterprise.context.Dependent;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
-import com.abraham.jsf.crud.local.jpa.JpaUtil;
 import com.abraham.jsf.crud.local.models.Empleado;
-import java.io.Serializable;
 
-@Dependent
-public class EmpleadoDAO implements Serializable{
+@Stateless
+public class EmpleadoDAO {
 
-    public void agregar(Empleado emp) {
-        EntityManager em = JpaUtil.getEntityManager();
-        try {
-            em.getTransaction().begin();
-            em.persist(emp);
-            em.getTransaction().commit();
-        } finally {
-            em.close();
-        }
-    }
+    @PersistenceContext
+    private EntityManager em;
 
     public Empleado obtener(int id) {
-        EntityManager em = JpaUtil.getEntityManager();
-        try {
-            return em.find(Empleado.class, id);
-        } finally {
-            em.close();
-        }
+        return em.find(Empleado.class, id);
     }
 
     public List<Empleado> listar() {
-        EntityManager em = JpaUtil.getEntityManager();
-        try {
-            return em.createQuery("SELECT e FROM Empleado e", Empleado.class).getResultList();
-        } finally {
-            em.close();
-        }
+        return em.createQuery("SELECT e FROM Empleado e", Empleado.class).getResultList();
+    }
+
+    public void agregar(Empleado emp) {
+        em.persist(emp);
     }
 
     public void actualizar(Empleado emp) {
-        EntityManager em = JpaUtil.getEntityManager();
-        try {
-            em.getTransaction().begin();
-            em.merge(emp);
-            em.getTransaction().commit();
-        } finally {
-            em.close();
-        }
+        em.merge(emp);
     }
 
     public void eliminar(int id) {
-        EntityManager em = JpaUtil.getEntityManager();
-        try {
-            em.getTransaction().begin();
-            Empleado emp = em.find(Empleado.class, id);
-            if (emp != null) {
-                em.remove(emp);
-            }
-            em.getTransaction().commit();
-        } finally {
-            em.close();
+        Empleado emp = em.find(Empleado.class, id);
+        if (emp != null) {
+            em.remove(emp);
         }
     }
 
     public boolean correoDuplicado(String correo, int idActual) {
-        EntityManager em = JpaUtil.getEntityManager();
-        try {
-            Long count = em.createQuery(
-                    "SELECT COUNT(e) FROM Empleado e WHERE e.correo = :correo AND e.id != :idActual", Long.class)
-                    .setParameter("correo", correo)
-                    .setParameter("idActual", idActual)
-                    .getSingleResult();
+        Long count = em.createQuery(
+                "SELECT COUNT(e) FROM Empleado e WHERE e.correo = :correo AND e.id != :idActual", Long.class)
+                .setParameter("correo", correo)
+                .setParameter("idActual", idActual)
+                .getSingleResult();
 
-            return count > 0;
-        } finally {
-            em.close();
-        }
+        return count > 0;
     }
+
 }
